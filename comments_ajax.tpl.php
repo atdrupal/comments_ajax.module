5cb5b07ebbe7333eced09ba4aa337539f4646ee2
duynguyen <duynguyen@duynguyen.(none)>
2012-12-25 21:59:53 +0700

    clear

--- comments_ajax.tpl.php
+++ comments_ajax.tpl.php
@@ -7,6 +7,7 @@
  * $profile
  */
 ?>
+
 <div class="view view-newsfeed-comment view-id-newsfeed_comment view-display-id-block_1 view-dom-id-f8d83bc3ef5aa88aeb66a8d2b6067c68">
 	<div class="view-content">
 		<div class="views-row views-row-1 views-row-odd views-row-first">
@@ -18,11 +19,11 @@
 						</a>
 					</div>
 					<div class="comment_detail"><a href="<?php print "/users/" . $user->uid?>" ><?php print $profile->title?></a>
-            <div class="comment_body"><?php $comment->comment ?></div>
+            <div class="comment_body"><?php print $comment->comment ?></div>
 					</div>
 					<div class="comment_info">
 						<ul>
-							<li class="date">Dec 24, 2012 at 3:00 PM</li>
+							<li class="date"><?php print date("D M j G:i:s", $profile->timestamp)?></li>
 							<li><a href="<?php print "/comment/reply/" . $comment->nid . '/' . $comment->cid ?>">Reply</a></li>
 							<li class="fb_like"><fb:like colorscheme="dark" font="verdana" action="like" width="350" show_faces="false" layout="button_count" href="http://staging.academicroom.com/node/"></fb:like></li>
 							<li><a>Report</a></li>
