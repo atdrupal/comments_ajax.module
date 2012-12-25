17a41d6cd588a8e31e03a022e8798cd8193e47da
duynguyen <duynguyen@duynguyen.(none)>
2012-12-25 10:18:52 +0700

    add ajax

--- comments_ajax.js
+++ comments_ajax.js
@@ -1,18 +1,19 @@
 // $Id$
 Drupal.behaviors.comments_ajax = function (context) {
-  $('a.categoryLink:not(.categoryLink-processed)', context).click(function () {
+  $('a.commentsLink:not(.commentsLink-processed)', context).click(function () {
     // This function will get exceuted after the ajax request is completed successfully
-    var updateProducts = function(data) {
-      // The data parameter is a JSON object. The “products” property is the list of products items that was returned from the server response to the ajax request.
-      $('#divProducts').html(data.products);
+    var updateComments = function(data) {
+      // The data parameter is a JSON object. The “comments” property is the list of products items that was returned from the server response to the ajax request.
+      $('#divComments').html(data.comments);
     }
     $.ajax({
       type: 'POST',
       url: this.href, // Which url should be handle the ajax request. This is the url defined in the <a> html tag
-      success: updateProducts, // The js function that will be called upon success request
+      success: updateComments, // The js function that will be called upon success request
       dataType: 'json', //define the type of data that is going to get back from the server
       data: 'js=1' //Pass a key/value pair
     });
     return false;  // return false so the navigation stops here and not continue to the page in the link
-}).addClass('categoryLink-processed');
+}).addClass('commentsLink-processed');
 }
+l(t('My link'),'comments/get' . $nid ,array('attributes' => array('class' => 'widelink','rel' => 'lightbox',)));
