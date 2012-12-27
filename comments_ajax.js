0dd62ba5402fe9f08e510c851c822ab72d3b5bc5
duynguyen <duynguyen@duynguyen.(none)>
2012-12-27 20:34:56 +0700

    clear

--- comments_ajax.js
+++ comments_ajax.js
@@ -5,9 +5,9 @@ Drupal.behaviors.comments_ajax = function (context) {
     var updateComments = function(data) {
       // The data parameter is a JSON object. The “comments” property is the list of products items that was returned from the server response to the ajax request.
       $("#" + "divComments-" + data.nid).html(data.comments);
+      alert(data.nid);
     }
     var args = this.href.split("/");
-    alert(args[args.length - 2]);
     $("#" + "my-loading-" + args[args.length - 2]).css('visibility','visible');
     $.ajax({
       type: 'POST',
