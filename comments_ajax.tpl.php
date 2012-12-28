6cb4ea334196dc83bef907b784eed077c526174c
duynguyen <duynguyen@duynguyen.(none)>
2012-12-28 11:21:05 +0700

    clear

--- comments_ajax.tpl.php
+++ comments_ajax.tpl.php
@@ -7,34 +7,6 @@
  * $profile
  */
 ?>
-
-<!--<div class="view view-newsfeed-comment view-id-newsfeed_comment view-display-id-block_1 view-dom-id-f8d83bc3ef5aa88aeb66a8d2b6067c68">
-	<div class="view-content">
-		<div class="views-row views-row-1 views-row-odd views-row-first">
-			<div class="views-field views-field-nothing">
-				<span class="field-content">
-					<div class="author_image">
-						<a target="_blank" href="<?php print "/users/" . $user->uid?>" >
-              <?php print $avatar?>
-						</a>
-					</div>
-					<div class="comment_detail"><a href="<?php print "/users/" . $user->uid?>" ><?php print $profile->title?></a>
-            <div class="comment_body"><?php print $comment->comment ?></div>
-					</div>
-					<div class="comment_info">
-						<ul>
-							<li class="date"><?php print date("M d, Y \a\t g:i A", $profile->timestamp)?></li>
-							<li><a href="<?php print "/comment/reply/" . $comment->nid . '/' . $comment->cid ?>">Reply</a></li>
-							<li class="fb_like"><fb:like colorscheme="dark" font="verdana" action="like" width="350" show_faces="false" layout="button_count" href="http://staging.academicroom.com/node/"></fb:like></li>
-							<li><a>Report</a></li>
-						</ul>
-					</div>
-				</span>
-      </div>
-    </div>
-  </div>
-</div>-->
-
 <div class="view view-newsfeed-comment view-id-newsfeed_comment view-display-id-block_1">
   <div class="view-content">
     <div class="item-list">    
